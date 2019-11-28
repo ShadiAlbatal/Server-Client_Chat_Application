edf9eb33c511cabb18350572d9b95fe3dc7e6e5f
shadialbatal <shadialbatal1993@hotmail.com>
2019-11-28 13:29:17 +0100

    small correction

--- UnitTest.py
+++ UnitTest.py
@@ -5,18 +5,16 @@ import datetime
 class Test_server (unittest.TestCase):
 
     HOST = socket.gethostname()
-    PORT = 12345
+    PORT = 1234
 
     def test_server(self):
 
         CONNECTION = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         CONNECTION.connect((self.HOST, self.PORT))
-
         MESSAGE = "Shadi"
         CONNECTION.send(MESSAGE.encode())
         rec = CONNECTION.recv(1024).decode()
         CONNECTION.close()
-
         self.assertEqual(MESSAGE, "Shadi")
         print(str(datetime.datetime.now()))
 
